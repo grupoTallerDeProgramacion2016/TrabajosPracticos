6ac64cc45020ee9aa9271fed467787abda42a626
Cabezon <ballesteros332@gmail.com>
2016-09-02 20:18:45 -0300

    Ej4 sin terminar

--- TP2/Ej4/Complejo.cs
+++ TP2/Ej4/Complejo.cs
@@ -11,6 +11,80 @@ namespace Ej4
         private double iReal;
         private double iImaginario;
 
+        public Complejo(double pReal, double pImaginario)
+        {
+            iReal = pReal;
+            iImaginario = pImaginario;
+        }
 
+        public double Real
+        {
+            get
+            {
+                return this.iReal;
+            }
+        }
+
+        public double Imaginario
+        {
+            get
+            {
+                return this.iImaginario;
+            }
+        }
+
+        public double ArgumentoEnRadianes
+        {
+            get
+            {
+                return Math.Atan2(this.iImaginario,this.iReal);
+            }
+        }
+
+        public double ArgumentoEnGrados
+        {
+            get
+            {
+                return 1.0;
+            }
+        }
+
+        public Complejo Conjugado
+        {
+            get
+            {
+                return this;
+            }
+        }
+
+        public double Magnitud
+        {
+            get
+            {
+                return Math.Sqrt(Math.Pow(iImaginario, 2) + Math.Pow(iReal, 2));
+            }
+        }
+
+        public bool EsReal()
+        {
+            return iImaginario == 0;
+        }
+
+        public bool EsImaginario()
+        {
+            return iImaginario != 0;
+        }
+
+        public bool EsIgual(Complejo pOtroComplejo)
+        {
+            return iImaginario == pOtroComplejo.Imaginario && iReal == pOtroComplejo.Real;
+        }
+
+        public bool EsIgual(double pReal, double pImaginario)
+        {
+            return iImaginario == pImaginario && iReal == pReal;
+        }
+
+        
     }
 }
