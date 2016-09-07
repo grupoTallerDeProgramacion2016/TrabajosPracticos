999179b3385af46fc856e7d04f577f1128880f36
luciorodriguez13 <luciorodriguez13@gmail.com>
2016-09-07 14:43:35 -0300

    Ej 5 muy avanzado - Lucio -

---
+++ TP2/Ej5/Fecha.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej5
+{
+    class Fecha
+    {
+        private static DateTime iFecha;
+        DateTime fecha = iFecha;
+        
+        public static void agregarDia(int pDia)
+        {
+            iFecha.AddDays(pDia);
+        }
+
+        public static void agregarMes(int pMes)
+        {
+            iFecha.AddMonths(pMes);
+        }
+
+        public static void agregarAño(int pAño)
+        {
+            iFecha.AddYears(pAño);
+        }
+
+        public int devolverDia
+        {
+            get { return iFecha.Day; }
+        }
+
+        public int devolverMes
+        {
+            get { return iFecha.Month; }
+        }
+
+        public int devolverAño
+        {
+            get { return iFecha.Year; }
+        }
+
+        public string devolverDiaDeLaSemana
+        {
+            get { return Convert.ToString(iFecha.DayOfWeek); }
+        }
+
+        public bool añoBisiesto
+        {
+            get { return DateTime.IsLeapYear(iFecha.Year) ; }
+        }
+
+
+        
+            
+        
+    }
+}
