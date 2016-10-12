fbc48faa8e953e7b246cdd2031ef4598a812cf77
luciorodriguez13 <luciorodriguez13@gmail.com>
2016-10-12 18:40:30 -0300

    Fabrica Encriptadores

--- TP3/Ej4/FabricaEncriptadores.cs
+++ TP3/Ej4/FabricaEncriptadores.cs
@@ -6,8 +6,57 @@ using System.Threading.Tasks;
 
 namespace Ej4
 {
-    class FabricaEncriptadores
-    {
+       public class FabricaEncriptadores
+        {
+            private static FabricaEncriptadores cInstancia;
+            private static readonly Object Critico = new object();
+            private Dictionary<String, IEncriptador> iEncriptadores;
 
-    }
-}
+            private FabricaEncriptadores()
+            {
+                Encriptador encriptador;
+                this.iEncriptadores = new Dictionary<string, IEncriptador>();
+                encriptador = new EncriptadorNulo();
+                this.iEncriptadores.Add(encriptador.Nombre, encriptador);
+                encriptador = new EncriptadorAES();
+                this.iEncriptadores.Add(encriptador.Nombre, encriptador);
+                encriptador = new EncriptadorCesar(3);
+                this.iEncriptadores.Add(encriptador.Nombre, encriptador);
+            }
+
+            public static FabricaEncriptadores Instancia
+            {
+                get
+                {
+                    if (cInstancia == null)
+                    {
+                        lock (Critico)
+                            if (cInstancia == null)
+                            {
+                                cInstancia = new FabricaEncriptadores();
+                            }
+
+                    }
+                    return cInstancia;
+                }
+            }
+
+            public IEncriptador GetEncriptador(string pNombre)
+            {
+                IEncriptador Encriptador;
+                if (!iEncriptadores.ContainsKey(pNombre))
+                    Encriptador = new EncriptadorNulo();
+
+                else
+                    Encriptador = iEncriptadores[pNombre];
+                return (Encriptador);
+            }
+
+
+
+
+        }
+ }
+
+
+
