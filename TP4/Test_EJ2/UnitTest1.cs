392cc3dd5b616930b701d26c7cabf8eb2a9c4414
luciorodriguez13 <luciorodriguez13@gmail.com>
2016-10-21 18:57:28 -0300

    Falta un solo test del ej2

--- TP4/Test_EJ2/UnitTest1.cs
+++ TP4/Test_EJ2/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Ej2;
 
 namespace Test_EJ2
 {
@@ -7,8 +8,26 @@ namespace Test_EJ2
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void SinExcepcion()
         {
+            double num1 = 0;
+            double num2 = 4;
+            Division division = new Division();
+            double resultado = division.obtenerDivision(num1, num2);
+            double resultadoEsperado = 0;
+            Assert.AreEqual(resultado, resultadoEsperado);
+                    
+        }
+
+        [TestMethod]
+        public void ConExcepcion()
+        {
+            double num1 = 0;
+            double num2 = 4;
+            Division division = new Division();
+            double resultado = division.obtenerDivision(num1, num2);
+            double resultadoEsperado = 0;
+            Assert.AreEqual(resultado, resultadoEsperado);
 
         }
     }
