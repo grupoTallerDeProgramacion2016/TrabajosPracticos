784f286295d285b77762065498c1f61c0d5275c6
luciorodriguez13 <luciorodriguez13@gmail.com>
2016-10-28 19:44:52 -0300

    Ej 7

---
+++ TP4/Ej7/Aplicacion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej7
+{
+    /// <summary>
+    /// Clase que representa la lógica del ejercicio (facade)
+    /// </summary>
+    class Aplicacion
+    {
+
+
+    }
+}
