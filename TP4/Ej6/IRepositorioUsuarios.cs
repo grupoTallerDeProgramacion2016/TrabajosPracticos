5dd39a1c2fcc724c0d5a6be46d4561d16b2dd001
lautaro08 <lautaro.zapata08@gmail.com>
2016-11-01 22:48:27 -0300

    Repositorio arreglado

---
+++ TP4/Ej6/IRepositorioUsuarios.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ej6
+{
+    interface IRepositorioUsuarios
+    {
+        void Agregar(Usuario pUsuario);
+        void Actualizar(Usuario pUsuario);
+        void Eliminar(string pCodigo);
+        List<Usuario> ObtenerTodos();
+        Usuario ObtenerPorCodigo(string pCodigo);
+        List<Usuario> ObtenerOrdenadoPor(IComparer<Usuario> comparador);
+    }
+}
